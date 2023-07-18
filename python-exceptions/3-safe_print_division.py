914fdbf88abbe0a46a20d19d1b6115d4036ae537
Enzobat0 <e.batungwan@alustudent.com>
2023-07-18 02:56:05 -0700

    .

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except (TypeError, ValueError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
