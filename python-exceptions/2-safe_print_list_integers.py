72d169195ac1724c5b33c9a53dd961874113abd2
Enzobat0 <e.batungwan@alustudent.com>
2023-07-18 01:37:08 -0700

    .

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
     try:
         for item in my_list[:x]:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(item), end="")
             nb_print += 1
     except (IndexError, TypeError, ValueError):
         pass
