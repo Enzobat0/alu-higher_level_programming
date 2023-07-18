4b2d13e3446cb44f63b4346e1e8c43ba273d89ef
Enzobat0 <e.batungwan@alustudent.com>
2023-07-18 02:34:55 -0700

    .

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_print += 1
             index += 1
     except (ValueError, TypeError):
-        pass
+        index += 1
     finally:
         print()
     return nb_print
