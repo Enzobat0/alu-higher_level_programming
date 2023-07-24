5ea2fca38b447efdf410bb0226793062825f49e7
Enzobat0 <e.batungwan@alustudent.com>
2023-07-24 07:12:05 -0700

    .

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,9 +8,11 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size = size
+    #property
     def size(self):
         return self.__size
-    defsize(self, value):
+    #size.setter
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
