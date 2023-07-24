2dcb6192448ee8b65ceb2e84fba92dc42794f625
Enzobat0 <e.batungwan@alustudent.com>
2023-07-24 07:30:31 -0700

    .

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,9 +7,11 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+
     #property
     def size(self):
         return self.__size
+
     #size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,6 +19,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         return self.__size = size
+
     def area(self):
         return self.__size ** 2
 
