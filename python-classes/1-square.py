1d3464a7ce2df34ab36959f195c517e13418bee7
Enzobat0 <e.batungwan@alustudent.com>
2023-07-24 01:43:23 -0700

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Square class with private instance size
+"""
 
 
 class Square:
+    """square class size is a private instance that why there's __ before size
+    """
     def __init__(self,size):
         self:__size = size
