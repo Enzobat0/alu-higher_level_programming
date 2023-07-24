b4c0bd18983a6d0e725bf10d4307ef6add4cf603
Enzobat0 <e.batungwan@alustudent.com>
2023-07-24 07:06:08 -0700

    .

---
+++ python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class Square
+"""
+
+
+class Square:
+    """Class square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+    def size(self):
+        return self.__size
+    defsize(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size = size
+    def area(self):
+        return self.__size ** 2
+
