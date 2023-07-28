99eb01647131085f7a40763bafdf4a5fc68b0e49
Enzobat0 <e.batungwan@alustudent.com>
2023-07-28 06:31:14 -0700

    .

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""n emptyclass quare"""
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """Initialize"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+        def area(self):
+        '''function to return the area of a rectangle'''
+        r_area = self.__width * self.__height
+        return r_area
