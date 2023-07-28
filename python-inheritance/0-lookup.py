09736ae6330a80864df6fa280b4a5deca3c70114
Enzobat0 <e.batungwan@alustudent.com>
2023-07-28 02:20:16 -0700

    .

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""A function that returns the list of available methods attribute"""
+
+
 def lookup(obj):
+    """dir is a built-in that returns everything within"""
+
     return dir(obj)
