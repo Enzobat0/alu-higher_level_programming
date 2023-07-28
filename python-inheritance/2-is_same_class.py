8d3b606dfc0468f955bbb64e82e64b922f84b320
Enzobat0 <e.batungwan@alustudent.com>
2023-07-28 02:50:14 -0700

    .

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that returns True if the object is exactly an instance of the specified class ; otherwise False."""
+
+
+def is_same_class(obj, a_class):
+    """Verify if the type of the on=bj coresponds to a_class"""
+    type(obj) == a_class
