702db5cdde825d7bc393bbb786d7e179935dc5b2
Enzobat0 <e.batungwan@alustudent.com>
2023-09-26 08:53:09 -0700

    .

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-if (!isNaN(arg) && arg != undefined){
-	const x = parseInt(arg, 10); // Convert to integer with base 10
-
-  if (x > 0) {
-    for (let i = 0; i < x; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
-  }
+const x = parseInt(process.argv[2]);
+let result;
+if (isNaN(x)) {
+  result = 'Missing number of occurrences';
+  console.log(result);
 } else {
-  console.log('Missing number of occurrences');
+  result = 'C is fun';
+  let i;
+  for (i = 0; i < x; i++) {
+    console.log(result);
+  }
 }
