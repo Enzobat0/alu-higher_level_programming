3d141256f1660ce10884ab52f2197e5c9c12228a
Enzobat0 <e.batungwan@alustudent.com>
2023-09-26 03:21:12 -0700

    .

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[2] !== undefined){
-	console.log(`First argument: ${firstArgument}`);
-}else{
-	console.log('No argument');
+if (process.argv.length <= 2) {
+    console.log('No argument');
+} else {
+    // Access and print the first argument
+    const firstArgument = process.argv[2];
+    console.log(`First argument: ${firstArgument}`);
 }
