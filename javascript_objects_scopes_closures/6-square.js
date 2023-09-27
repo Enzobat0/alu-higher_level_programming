15557c3a475feac2db1746d0f916731794ca6d6c
Enzobat0 <e.batungwan@alustudent.com>
2023-09-27 05:06:39 -0700

    .

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Square_ = require('./5-square');
 
 class Square extends Square_ {
   charPrint (c = 'X') {
-    for (let cont = 0, cont < this.size, cont++) {
+    for (let cont = 0; cont < this.size; cont++) {
       console.log(c.repeat(this.size));
     }
   }
